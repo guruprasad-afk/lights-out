a5d720ccb2fbaabb40a977bfab0d249ac59632c2
Guru Prasad <55026691+guruprasad-afk@users.noreply.github.com>
2020-03-15 13:49:45 +0530

    Updates

---
+++ precache-manifest.15e66bc56701148a9ec4629df4aa6484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "773330b5c776afe5d953",
+    "url": "/lights-out/static/css/main.9312f331.chunk.css"
+  },
+  {
+    "revision": "773330b5c776afe5d953",
+    "url": "/lights-out/static/js/main.773330b5.chunk.js"
+  },
+  {
+    "revision": "306596ad124034e94b54",
+    "url": "/lights-out/static/js/1.306596ad.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "8b6aa8d367e412ba26a744ea0ed3ec4c",
+    "url": "/lights-out/index.html"
+  }
+];
